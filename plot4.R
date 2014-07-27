b47f35d9c7e0466d2266bc72074417cadf30cf15
DoubleDFresh <duncan.doug@gmail.com>
2014-07-27 12:53:47 -0500

    adding plot4.r

--- plot4.R
+++ plot4.R
@@ -7,31 +7,30 @@ library(data.table)
 library(RColorBrewer)
 library(ggplot2)
 library(plyr)
+library(scales)
 
 #  First, read in data from working directory using readRDS().  Assumes file is in unzipped in present working dir.
 dt <- data.table(readRDS("summarySCC_PM25.rds"))
 dt2 <- data.table(readRDS("Source_Classification_Code.rds"), stringsAsFactors=FALSE)
-names(dt)
-names(dt2)
-dt2 <- dt2[, SCC:=as.character(SCC)]
+
 #  Now, subset the data using grep looking for coal emissions to merge w/ dt
 dt2.sub <- dt2[grep("Coal$", dt2$EI.Sector), ]
-names(dt2.sub)
 
+dt <- merge(dt,dt2.sub,by="SCC")  
 
-## 2.  Subset using SCC
 
 #  Set year and type as factors for ggplot2
-dt.sub$year <- as.factor(dt.sub$year)
-dt.sub$type <- as.factor(dt.sub$type)
+dt$year <- as.factor(dt$year)
+dt$type <- as.factor(dt$type)
 
-df <- aggregate(dt.sub$Emissions, by=list(Year=dt.sub$year, Type =dt.sub$type), FUN=sum)
+#  Aggregate for ggplot2
+df <- aggregate(dt$Emissions, by=list(Year=dt$year, Type =dt$type), FUN=sum)
 
 
-png(file = "plot3.png", width = 480, height = 480)
+png(file = "plot4.png", width = 480, height = 480)
 ggplot(data=df, aes(x=Year, y=x, fill=Type)) + geom_bar(stat="identity", position=position_dodge()) +
         scale_fill_hue(name="Emission Type") +
         xlab("Year PM2.5 Emissions Recorded") + ylab("Tons of PM2.5 Emissions") +  
-        ggtitle("PM2.5 Emissions in Baltimore, MD by Year")
+        ggtitle("Coal PM2.5 Emissions in the US by Year and Type") + scale_y_continuous(labels = comma, limits = c(0,600000))
 
 dev.off()
